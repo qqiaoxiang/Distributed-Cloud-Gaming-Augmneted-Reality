e8d618cbdc2d5cf280156838be6ef59a4c080cb6
CaiGou <73175429+qqiaoxiang@users.noreply.github.com>
2022-05-14 00:16:11 +0100

    Test cuda is available by pytorch

--- torchtest.py
+++ torchtest.py
@@ -1,9 +1,12 @@
 import torch
 import torchvision
+
 print(torch.__version__,torch.cuda.is_available())
 print(torchvision.__version__)
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
+
 print(torch.cuda.get_device_name(0))
 print(torch.version.cuda)
 print(torch.backends.cudnn.version())

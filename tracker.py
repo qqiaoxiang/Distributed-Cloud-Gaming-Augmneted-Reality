266b04a3feadf516d593eb9f797af5b3af5ec9a5
CaiGou <73175429+qqiaoxiang@users.noreply.github.com>
2022-05-14 01:47:16 +0100

    Marks the target detection box

--- tracker.py
+++ tracker.py
@@ -19,11 +19,13 @@ def plot_bboxes(image, bboxes, line_thickness=None):
     # Plots one bounding box on image img
     tl = line_thickness or round(
         0.002 * (image.shape[0] + image.shape[1]) / 2) + 1  # line/font thickness
+    
     for (x1, y1, x2, y2, cls_id, pos_id) in bboxes:
         if cls_id in ['person']:
             color = (0, 0, 255)
         else:
             color = (0, 255, 0)
+            
         c1, c2 = (x1, y1), (x2, y2)
         cv2.rectangle(image, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
         tf = max(tl - 1, 1)  # font thickness
